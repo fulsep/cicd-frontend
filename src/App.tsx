61cc812bfc488dc007f8287d2dd7f448939bdaf8
fulsep <57238803+fulsep@users.noreply.github.com>
2025-02-18 09:32:45 +0000

    feat: configure router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Dashboard from './app/dashboard/page'
+import Login from './app/login/page'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+])
 
 function App() {
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
